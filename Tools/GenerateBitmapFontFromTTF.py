3bd81e95b411b1e8b28cd1695e098e7dfa4752a3
Mingjie Li <limingjie@outlook.com>
2023-04-03 17:22:21 +0800

    Format code to make the style align everywhere

--- Tools/GenerateBitmapFontFromTTF.py
+++ Tools/GenerateBitmapFontFromTTF.py
@@ -3,27 +3,24 @@ import numpy
 import sys
 
 
-# Convert character from TTF to bitmap
-# fontSize       - font height, 1 pixel
-# width & height - bitmap size
-def char_to_bitmap(char: str, fontFile: str, fontSize: int, bitmapSize: tuple[int, int]):
+def char_to_bitmap(char: str, font_file: str, font_size: int, bitmap_size: tuple[int, int]):
     """Convert character from TTF to bitmap
 
     Keyword arguments:
-    char       -- the character to convert
-    fontFile   -- the font file path
-    fontSize   -- the font size, usually 1 pixel smaller than height to leave character spacing and line spacing
-    bitmapSize -- A tuple of (width, height)
+    char        -- the character to convert
+    font_file   -- the TrueType font file
+    font_size   -- the font size, usually 1 pixel smaller than height to leave character spacing and line spacing
+    bitmap_size -- A tuple of (width, height)
     """
 
     # Create a new black and white image
-    img = Image.new('1', bitmapSize, color=1)
+    img = Image.new('1', bitmap_size, color=1)
 
     # Get a drawing context for the image
     draw = ImageDraw.Draw(img)
 
     # Draw the character in the center of the image
-    font = ImageFont.truetype(fontFile, fontSize)
+    font = ImageFont.truetype(font_file, font_size)
     draw.text((0, 0), char, font=font, fill=0)
 
     # Convert the image to a numpy array
@@ -34,43 +31,45 @@ def char_to_bitmap(char: str, fontFile: str, fontSize: int, bitmapSize: tuple[in
 
     # Convert the bitmap line by line into a uint8_t array
     bytes = []
-    for x in range(bitmapSize[0]):
+    for x in range(bitmap_size[0]):
         bits = 0
-        for y in range(bitmapSize[1]):
+        for y in range(bitmap_size[1]):
             if arr[y][x] == 0:
                 bits |= (1 << y)
-        for i in range (bitmapSize[1] // 8):
+        for i in range(bitmap_size[1] // 8):
             bytes.append(bits & 0xFF)
             bits >>= 8
 
     # Convert the array to uint8_t array
     count = 0
-    output_str = ""
+    output = ""
     for byte in bytes:
         if count % 16 == 0:
-            output_str += "\n    "
-        output_str +=f'0x{byte:02x}, '
+            output += "\n    "
+        output += f'{byte:#04x}, '
         count += 1
 
     # Append the character and hex value
-    output_str += "// '" + char + "' 0x" + (f'{ord(char):02x}' if ord(char) < 256 else f'{ord(char):04x}')
+    output += f"// '{char}' {ord(char):#0{4 if ord(char) < 256 else 6}x}"
 
-    return output_str
+    return output
+
 
 def main():
     output = "{"
 
-    # ASCII printable characters from space (0x20) to ~ (0x7E)
+    # Printable ASCII characters from space (0x20) to ~ (0x7E)
     for char in r""" !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_'abcdefghijklmnopqrstuvwxyz{|}~""":
         output += char_to_bitmap(char, "iosevka-term-regular.ttf", 8, (4, 8))
 
-    # Other Unicode characters
-    # for char in r"""微处理器""": # "微處理器" / "マイクロプロセッサ"
+    # Unicode characters
+    # for char in r"""微处理器""":
     #     output += char_to_bitmap(char, "sarasa-mono-sc-regular.ttf", 31, (32, 32))
 
     output += "\n}"
 
     print(output)
+
 
 if __name__ == "__main__":
     main()
